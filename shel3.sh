fcf7df0f3de30402c54a22e97da6abe9c26e61d5
Juyel <jhaqmd@gmail.com>
2020-02-14 17:55:06 +0000

    addition and multiplication

---
+++ shel3.sh
@@ -0,0 +1,6 @@
+echo "Enter 1st number"
+read a
+echo "Enter 2nd number"
+read b
+c=`expr $a + $b`
+echo $c
---
+++ shel4.sh
@@ -0,0 +1,6 @@
+echo "enter 1st number="
+read a
+echo "enter 2nd number="
+read b
+c=`expr $a \* $b`
+echo $c
